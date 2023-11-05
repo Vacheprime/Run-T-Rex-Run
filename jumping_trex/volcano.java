e18ca00b9f8361d37c5a67790f14ba42f2588b8e
DanatAliMuradov <2357647@edu.vaniercollege.qc.ca>
2023-11-05 11:21:56 -0500

    Adding the project source files and the background image of the game.

---
+++ jumping_trex/volcano.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class volcano here.
+ * 
+ * @author Shaheryar Anwar and Danat Ali Muradov 
+ * @version 1
+ */
+public class volcano extends World
+{
+
+    /**
+     * Constructor for objects of class volcano.
+     * 
+     */
+    public volcano()
+    {    
+        // Create a new volcano world with 600x400 cells with a cell size of 1x1 pixels.
+        super(500, 600, 1); 
+    }
+}
