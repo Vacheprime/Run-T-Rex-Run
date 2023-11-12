a7262efca2f4e17ecc3d22249b921a793dc4c379
DanatAliMuradov <2357647@edu.vaniercollege.qc.ca>
2023-11-12 09:32:51 -0500

    Minor code cleanup in the TRex class

--- jumping_trex/TRex.java
+++ jumping_trex/TRex.java
@@ -26,20 +26,21 @@ public class TRex extends Actor
     private static final double GRAVITY = 9.8 * 100; // 100 px = 1 m
     private static final int MAX_Y_VEL = 450;
     
-    public TRex() {
+    public TRex()
+    {
         // Scale the T-Rex to 1/4 of its original size
         GreenfootImage img = getImage();
         img.scale(img.getWidth()/4, img.getHeight()/4);
         setImage(img);
         
         // Initialize the TRex
-        position = null;
-        velocity = new Vector2D(0,0);
-        acceleration = new Vector2D(0, GRAVITY);
+        this.position = null;
+        this.velocity = new Vector2D(0,0);
+        this.acceleration = new Vector2D(0, GRAVITY);
         
         // Set the fields to the width and height of the scaled image
-        width = img.getWidth();
-        height = img.getHeight();
+        this.width = img.getWidth();
+        this.height = img.getHeight();
     }
     
     /**
@@ -54,6 +55,10 @@ public class TRex extends Actor
         }
         // Execute the movements
         moveTRex();
+        
+        // Update the physics of the T-Rex
+        updatePhysics();
+        
         // Detect collision with lava
         if (detectLavaCollision())
         {
@@ -63,97 +68,109 @@ public class TRex extends Actor
         }
     }
     
-    public void moveTRex()
+    private void moveTRex()
     {
         // When key d or right arrow is pressed
-        if (Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")) {
-            if (facing != 'r') {
+        if (Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right"))
+		{
+            if (facing != 'r')
+            {
                 // Change the orientation of the T-Rex to the right
                 mirrorImage('h');
                 facing = 'r';
             }
             
-            if (velocity.getX() != 5) {
+            if (velocity.getX() != 5)
+            {
                 velocity.setX(runVelocity);
             }
         
         // When key a or left arrow is pressed
-        } else if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")) {
-            if (facing != 'l') {
+        } 
+        else if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left"))
+        {
+            if (facing != 'l')
+            {
                 // Chand the orientation of the T-Rex to the left
                 mirrorImage('h');
                 facing = 'l';
             }
             
-            if (velocity.getX() != -5) {
+            if (velocity.getX() != -5)
+            {
                 velocity.setX(-runVelocity);
             }
         
-        } else {
-            if (velocity.getX() != 0) {
-                velocity.setX(0);
-            }
+        } else if (velocity.getX() != 0)
+        {
+			velocity.setX(0);
         }
         
-        // When key w or up arrow is pressed
-        if ( (Greenfoot.isKeyDown("w") || Greenfoot.isKeyDown("up") ) && velocity.getY() == 0) {
+        // When key w or up arrow is pressed and the player is not mid-air
+        if ( (Greenfoot.isKeyDown("w") || Greenfoot.isKeyDown("up") ) && acceleration.getY() == 0)
+        {
             velocity = Vector2D.add(velocity, jumpVelocity); 
         }
-        
-        // Update the physics of the T-Rex
-        updatePhysics();
     }
     
-    public void updatePhysics()
+    private void updatePhysics()
     {
         // Detect collisions on platforms and stop the TRex from falling
         int toRelocate = detectPlatformCollisions();
-        if (toRelocate != -1)
+        if (toRelocate != -1 && acceleration.getY() != 0)
         {
+			// Only relocate if the T-Rex is falling and not standing still
+			// on the platform
             if (acceleration.getY() != 0)
             {
                 acceleration.setY(0);
                 velocity.setY(0);
                 position.setY(toRelocate);
             }
-        } else
+        
+        // If there are no collisions, then gravity must be present
+        } else if (acceleration.getY() != GRAVITY)
         {
             acceleration.setY(GRAVITY);
         }
         
+        // Detect collisions with the volcano borders
         toRelocate = detectBorderCollision();
         if (toRelocate != -1)
         {   
-            if (toRelocate < getWorld().getWidth() / 2) {
-                if (velocity.getX() < 0)
-                {   
-                    velocity.setX(0);
-                }
-            } 
-            else
+			// If it is a collision on the left border
+            if (toRelocate < getWorld().getWidth() / 2)
             {
-                if (velocity.getX() > 0) {
-                    velocity.setX(0);
-                }
+				if (velocity.getX() < 0)
+				{
+					velocity.setX(0);
+				}
+            } 
+            else if (velocity.getX() > 0)
+            {
+                velocity.setX(0);
             }
             position.setX(toRelocate);
         }
-        // Update the position of the T-Rex
+        
+        // Update the position of the T-Rex on the screen
         updatePosition();
     }
     
-    public void mirrorImage(char side)
+    private void mirrorImage(char side)
     {
-        switch (side) {
-            
-            case 'h': {
+        switch (side)
+        {    
+            case 'h':
+            {
                 // Flip the image horizontally
                 GreenfootImage img = getImage();
                 img.mirrorHorizontally();
                 setImage(img);
                 break;
             }
-            case 'v': {
+            case 'v':
+            {
                 // Flip the image vertically
                 GreenfootImage img = getImage();
                 img.mirrorVertically();
@@ -164,14 +181,15 @@ public class TRex extends Actor
     }
     
 
-    public int detectPlatformCollisions()
+    private int detectPlatformCollisions()
     {
         // Get all platforms
         List<Platform> platformObjects = getWorld().getObjects(Platform.class);
         int positionToSurface = -1;
         
         // Loop for every platform
-        for (int i = 0; i < platformObjects.size(); i++) {
+        for (int i = 0; i < platformObjects.size(); i++)
+        {
             Platform platform = platformObjects.get(i);
             
             // get the half height and width of the platforms
@@ -183,76 +201,85 @@ public class TRex extends Actor
             int minimalYDist = pfHalfHeight + (height / 2);
             
             // The actual distance between the two actors
-            int distX = platform.getX() - getX();
-            int distY = platform.getY() - getY();
+            int distX = Math.abs(platform.getX() - getX());
+            int distY = Math.abs(platform.getY() - getY());
             
             // Check for collisions
-            if (Math.abs(distX) <= minimalXDist && Math.abs(distY) <= minimalYDist) {
-                if (velocity.getY() > 0  && (getY() + height / 2) < platform.getY()) {
-                    return platform.getY() - minimalYDist;
+            if (distX <= minimalXDist && distY <= minimalYDist)
+            {
+                // Only a collision if the T-Rex is falling from above
+                if (velocity.getY() > 0  && (getY() + height / 2) < platform.getY())
+                {
+                    positionToSurface = platform.getY() - minimalYDist;
                 }
             }
         }
-        
+        // Return where the T-Rex should be on the Y axis
         return positionToSurface;
     }
     
-    public int detectBorderCollision()
+    private int detectBorderCollision()
     {
-        final double leftBorderDistance = 50 * 1.5; // The distance of the border from the edge of the screen 
-        final double rightBorderDistance = getWorld().getWidth() - leftBorderDistance;
+		// The distance of the left border from the edge of the screen 
+        double leftBorderDistance = 50 * 1.5;
+        
+        // The distance of the right border from the edge of the screen
+        double rightBorderDistance = getWorld().getWidth() - leftBorderDistance;
         
         int positionToBorder = -1;
         if (getX() - width/2 <= leftBorderDistance)
         {
             positionToBorder = (int) leftBorderDistance + width/2;
-        } 
-        else if (getX() + width/2 >= rightBorderDistance) 
+        } else if (getX() + width/2 >= rightBorderDistance) 
         {
             positionToBorder = (int) rightBorderDistance - width/2;
         }
         
+        // Return where the T-Rex should be on the X axis
         return positionToBorder;
     }
     
-    public boolean detectLavaCollision() {
+    private boolean detectLavaCollision() {
         
-        int playerOuterBound = getY() + height/2;
+        // The Y location of the lava and of the upper bound of the T-Rex
+        int playerUpperBound = getY() + height/2;
         int currentLavaLevel = getWorld().getObjects(Lava.class).get(0).getLavaLevel();
         
-        if (playerOuterBound >= currentLavaLevel && currentLavaLevel != -1)
+        if (playerUpperBound >= currentLavaLevel && currentLavaLevel != -1)
         {
             return true;
-        } else {
+        } else
+        {
             return false;
         }
     }
     
-    public void updatePosition()
+    private void updatePosition()
     {
-        // Initial position
+        // Set the initial position
         if (position == null)
         {
             position = new Point2D(getX(), getY());
         }
         
-        
+        // Get the next position
         position = predictNextPosition();
         
-        // Set new actor position
+        // Set the position of the actor
         setLocation((int) position.getX(), (int) position.getY());        
     }
     
-    public Point2D predictNextPosition()
+    private Point2D predictNextPosition()
     {
         // Get time step duration
         Volcano world = (Volcano) getWorld();
         double dt = world.getTimeStepDuration();
         
-        // Update Y velocity
+        // Update the Y velocity
         Vector2D velocityVariation = Vector2D.multiply(acceleration, dt);
         
         velocity = Vector2D.add(velocity, velocityVariation);
+        
         // Make sure the Y velocity does not exceed the maximum
         if (velocity.getY() > MAX_Y_VEL)
         {
@@ -272,7 +299,7 @@ public class TRex extends Actor
         position.setY(getY() + scrollingSpeed);
     }
     
-    public void changeWorld(World world)
+    private void changeWorld(World world)
     {
         Greenfoot.setWorld(world);
     }
